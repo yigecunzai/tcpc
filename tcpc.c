f5545a963c565a05877e31269fd15a8f780be87d
robert.caston@gmail.com <robert.caston@gmail.com>
2008-08-08 10:58:11 -0700

    GPLv2-ing code

--- tcpc.c
+++ tcpc.c
@@ -1,9 +1,22 @@
 /*
- * tcpc.c
+ * tcpc.c - Main source file for the TCPC framework.
  *
- * AUTHOR:      Robert C. Curtis
+ * This file is part of TCPC.
  *
- * DESCRIPTION: TCPC is a threaded TCP server/client framework.
+ * Copyright (C) 2008 Robert C. Curtis
+ *
+ * TCPC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * TCPC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TCPC.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /****************************************************************************/
